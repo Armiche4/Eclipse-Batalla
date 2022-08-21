ac6f1efb17799d7e696cc200e652321f01414222
Guille <guillermomaceiras@hotmail.com>
2022-08-21 18:23:12 +0100

    fix routes errors

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import "./components/formulario.js";
 import "./components/formularioSimple.js";
 import "./components/navesSeleccionadas.js";
 import "./components/batalla.js";
-import "./components/caracteristicasNaves";
+import "./components/caracteristicasNaves.js";
